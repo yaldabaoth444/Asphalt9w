5c13a8a0f6f7e85603fd867f2dfbf3ac79b2fedf
yaldabaoth444 <25618671+yaldabaoth444@users.noreply.github.com>
2021-02-22 20:15:29 +0500

    Update Start.py

--- Start.py
+++ Start.py
@@ -2,15 +2,17 @@ import sys
 from Bot import Bot
  
 if __name__ == "__main__":
-    try:
-        input("Press Enter to continue...")
-        bot = Bot()
-        if len(sys.argv) > 1:
-            bot.Run(sys.argv[1])
-        else:    
-            bot.Run('NETWORK')
-    except:
-        sys.exit(1)
-        
-    
-     
+	try:
+		input("Press Enter to continue...")
+		sys.stdout.flush()
+		print('Press «Esc» to pause or «Q» to quit')
+		
+		if len(sys.argv) == 2:
+			bot = Bot(sys.argv[1])
+		elif len(sys.argv) == 3:
+			bot = Bot(sys.argv[1], sys.argv[2])
+		else:
+			bot = Bot()
+		bot.Run()
+	except:
+		sys.exit(1)
